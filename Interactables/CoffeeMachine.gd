b3284aef59dc7e8ced956ba585b28e9dae9637f0
bilou84 <nicolas.gauthier84@gmail.com>
2022-01-29 20:47:20 +0100

    Implement coffee machine interaction

--- Interactables/CoffeeMachine.gd
+++ Interactables/CoffeeMachine.gd
@@ -2,5 +2,5 @@ extends BaseInteractable
 
 func interact():
 	.interact()
-	get_node("/root/Spatial/Televisionv04/Screen").visible = false
+	$Particles.emitting = true
 	get_node("/root/Spatial/Player").incrementProgress()
