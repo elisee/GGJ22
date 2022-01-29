76b0a78e76d0f674c7424bcb8fa2a19afeb7607b
Elisée Maurer <elisee@sparklinlabs.com>
2022-01-30 00:32:40 +0100

    Disable TV light when switching off the TV

--- Interactables/Remote.gd
+++ Interactables/Remote.gd
@@ -5,5 +5,10 @@ onready var television = get_tree().get_nodes_in_group("television")[0]
 
 func interact():
 	.interact()
+	
+	var tvLight = get_tree().find_node("Light TV")
+	if tvLight != null:
+		tvLight.visible = false
+	
 	television.get_node("Screen").visible = false
 	player.increment_progress()
