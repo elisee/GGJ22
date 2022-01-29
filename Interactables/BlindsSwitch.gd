83b631a6f6c8b18deb7fef5df640ed11bd71eebd
Elisée Maurer <elisee@sparklinlabs.com>
2022-01-30 00:03:31 +0100

    Make blinds go up

--- Interactables/BlindsSwitch.gd
+++ Interactables/BlindsSwitch.gd
@@ -3,10 +3,22 @@ extends BaseInteractable
 onready var player = get_tree().get_nodes_in_group("player")[0]
 onready var blinds = get_tree().get_nodes_in_group("blinds")[0]
 
+var active = false
+
 func interact():
 	.interact()
 	
 	$Switch_Button.rotation_degrees.x = 20
-	
-	# blinds.get_node("Screen").visible = false
+	self.active = true
+
 	player.increment_progress()
+
+func _process(delta):
+	if not self.active:
+		return
+	
+	var blind1 = blinds.get_node("Blind_01") as Spatial
+	var blind2 = blinds.get_node("Blind_02") as Spatial
+	
+	blind1.translation.y = lerp(blind1.translation.y, 13, delta * 0.7)
+	blind2.translation.y = lerp(blind2.translation.y, 13.3, delta * 1.2)
