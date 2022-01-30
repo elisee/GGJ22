a4d9a0cadef1d31bb1125235f9085e72d8cb99bc
bilou84 <nicolas.gauthier84@gmail.com>
2022-01-30 13:36:25 +0100

    Make the play button in the menu load the final map

--- Menus/MainMenu.gd
+++ Menus/MainMenu.gd
@@ -1,25 +1,10 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$RootVBoxContainer/ButtonsVBoxContainer/PlayButton.grab_focus()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_PlayButton_pressed():
-	get_tree().change_scene("res://House/House.tscn")
-
+	get_tree().change_scene("res://Dev/Mord/map2.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
